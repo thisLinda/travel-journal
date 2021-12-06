24a6254e929291385738662da98cfa7f36b7b101
linda_forlizzi <la.for.lizzi@gmail.com>
2021-12-05 20:05:04 -0500

    added the beer

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,12 +8,15 @@ export default function Card(props) {
                     <h2 className='card--pin-location'>{props.location}</h2>
                     <a href={props.googleMapsUrl}
                         className='card--pin-link'
-                        target='_blank'>
+                        target='_blank'
+                        rel='noreferrer'>
                         View on Google maps</a>
                 </div>
                 <h1 className='card--info-location'>{props.title}</h1>
                 <h3 className='card--info-date'>{props.moyy}</h3>
-                <p className='card--info-text'>{props.description}</p>
+                <p className='card--info-text'>{props.description}
+                </p>
+                <p className='card--info-beer'>The beer: {props.beer}</p>
             </div>
         </div>
     )
